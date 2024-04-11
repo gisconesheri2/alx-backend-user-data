89fbda854525ca500cd44f234b58d8b554cc8561
gisconesheri2 <pgikonyo2022@gmail.com>
2024-04-11 21:58:05 +0300

    fix bytes for bcrypt

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -5,12 +5,14 @@ import bcrypt
 
 def hash_password(password: str) -> bytes:
     """Hash a password"""
-    hash_pd = bcrypt.hashpw(password, bcrypt.gensalt())
+    hash_pd = bcrypt.hashpw(bytes(password, encoding='utf-8'),
+                            bcrypt.gensalt())
     return hash_pd
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
     """check if a password is valid"""
-    if bcrypt.checkpw(password, hashed_password):
+    pd = (bytes(password, encoding='utf-8'))
+    if bcrypt.checkpw(pd, hashed_password):
         return True
     return False
