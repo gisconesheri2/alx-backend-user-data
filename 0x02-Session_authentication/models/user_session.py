6e8b100718c658763d6e68d10338b466dc6b902e
gisconesheri2 <pgikonyo2022@gmail.com>
2024-04-18 21:09:30 +0300

    advanced tasks

--- 0x02-Session_authentication/models/user_session.py
+++ 0x02-Session_authentication/models/user_session.py
@@ -6,9 +6,11 @@ from models.base import Base
 
 
 class UserSession(Base):
+    """Model a userSession database storage model"""
 
     def __init__(self, *args: list, **kwargs: dict):
-        """Initialize the class"""
+        """Initialize the class
+        and set user_id and session_id"""
         super().__init__(args, kwargs)
         self.user_id = kwargs.get('user_id')
         self.session_id = kwargs.get('session_id')
