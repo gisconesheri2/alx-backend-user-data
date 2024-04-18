087927213a97cb8461d5d1ab3ae17a598d12a050
gisconesheri2 <pgikonyo2022@gmail.com>
2024-04-18 23:07:31 +0300

    fix typing

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -28,27 +28,6 @@ def filter_datum(fields: List[str], redaction: str,
     return message
 
 
-class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class
-        """
-
-    REDACTION = "***"
-    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
-    SEPARATOR = ";"
-
-    def __init__(self, fields: List[str]):
-        """Initialize the class"""
-        super(RedactingFormatter, self).__init__(self.FORMAT)
-        self.fields = fields
-
-    def format(self, record: logging.LogRecord) -> str:
-        """Format a log record y redacting any personal info"""
-        redacted_msg = filter_datum(self.fields, self.REDACTION,
-                                    record.msg, self.SEPARATOR)
-        record.msg = redacted_msg
-        return super().format(record)
-
-
 def get_logger() -> logging.Logger:
     """
     Set up handlers and formatters for the logger
@@ -99,6 +78,27 @@ def main():
         row_elems = ([str(entry) for entry in row])
         rhe = list(zip(row_head, row_elems))
         logger.info('; '.join(['='.join(elem) for elem in rhe]))
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """Initialize the class"""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Format a log record y redacting any personal info"""
+        redacted_msg = filter_datum(self.fields, self.REDACTION,
+                                    record.msg, self.SEPARATOR)
+        record.msg = redacted_msg
+        return super().format(record)
 
 
 if __name__ == '__main__':
